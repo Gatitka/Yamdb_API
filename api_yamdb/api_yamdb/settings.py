0cb288cc362683b4b7a18c50f3f5b39e29353b2e
Александр Лопата <alex.lopata2012@yandex.ru>
2022-12-31 00:50:20 +0300

    Users app started and registered.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,6 +18,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
