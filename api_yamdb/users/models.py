76d7cd31037be01260f70322418260ab505071aa
Александр Лопата <alex.lopata2012@yandex.ru>
2023-01-02 20:12:00 +0300

    Created new User model. Created and applied migrations.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER_ROLES = (
+    ('ADM', 'admin'),
+    ('MOD', 'moderator'),
+    ('USR', 'user'),
+)
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'О себе',
+        blank=True
+    )
+    role = models.CharField(
+        'Роль',
+        max_length=3,
+        choices=USER_ROLES,
+        default='USR'
+    )
