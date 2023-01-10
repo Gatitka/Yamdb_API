9ea972eeebb7590e12510b58f3d607e882c64ecc
Александр Лопата <alex.lopata2012@yandex.ru>
2023-01-10 23:49:02 +0300

    Small changes in existing custom commands. Created command for load users data. Created command that run all commands together.

--- api_yamdb/reviews/management/commands/load_category_data.py
+++ api_yamdb/reviews/management/commands/load_category_data.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         with open('./static/data/category.csv', encoding='utf-8') as f:
             for row in DictReader(f):
                 category = Category(
+                    id=row['id'],
                     name=row['name'],
                     slug=row['slug']
                 )
--- api_yamdb/reviews/management/commands/load_genre_title_data.py
+++ api_yamdb/reviews/management/commands/load_genre_title_data.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
         with open('./static/data/genre_title.csv', encoding='utf-8') as f:
             for row in DictReader(f):
                 genretitle = GenreTitle(
+                    id=row['id'],
                     genre_id=row['genre_id'],
                     title_id=row['title_id']
                 )
